4aacc0209347359c2df7e970b365d01f9557a4c8
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-06 02:39:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_fourth_tier
   mwtt[:railroads][:names][:reading_railroad] = {:morgage_value => 100}
   mwtt[:railroads][:names][:pennsylvania_railroad] = {:morgage_value => 200}
   mwtt[:railroads][:names][:b_and_o_railroad] = {:morgage_value => 400}
-  monopoly_with_third_tier[:railroads][:names][:shortline_railroad] = {:morgage_value => 800}
+  mwtt[:railroads][:names][:shortline_railroad] = {:morgage_value => 800}
 end
 
 monopoly_with_second_tier
