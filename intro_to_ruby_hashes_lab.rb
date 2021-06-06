90d312e95455d8578e397763fd28125995818a54
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-06 02:35:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,30 @@
-def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
 
+def base_hash
+  {:railroads => {}}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  base_hash[:railroads] = 
+  {
+    :pieces => 4,
+    :rent_in_dollars => {},
+    :names => {}
+  }
 
+  base_hash
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  
+  monopoly_with_second_tier[:railroads][:rent_in_dollars] = {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
 
+  monopoly_with_second_tier[:railroads][:names] = {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {}}
+  
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-
+  monopoly_with_third_tier[:railroads][:names][:reading_railroad] = {:morgage_value => 100}
+  monopoly_with_third_tier[:railroads][:names][:pennsylvania_railroad] = {:morgage_value => 200}
+  monopoly_with_third_tier[:railroads][:names][:b_and_o_railroad] = {:morgage_value => 400}
+  monopoly_with_third_tier[:railroads][:names][:shortline_railroad] = {:morgage_value => 800}
 end
